35971d7466d1153dfa8eceecc290f0d9bf3cdd61
shenli <shenli1120@qq.com>
2014-08-08 13:14:04 +0800

    修改channelUrl由外部传入

--- public/webrtc-client.js
+++ public/webrtc-client.js
@@ -3,68 +3,68 @@ var URL = window.URL || window.webkitURL || window.msURL || window.oURL;
 var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 var RTCSessionDescription = RTCSessionDescription || mozRTCSessionDescription;
 (function(exports) {
-		function RTCClient() {
-			this.SERVER = {
-				iceServers: [{
-					url: "stun:" + location.hostname + ":9001"
-				}]
-			};
-			//createOffer createAnswer 第三个参数RTCOfferOptions
-			var mediaConstraints = {
-				optional: [],
-				mandatory: {
-					OfferToReceiveAudio: true,
-					OfferToReceiveVideo: true
-				}
-			};
-			this.eventHandlers = {};
-			this.channel = null;
-			this.localStreams = [];
-			this.peerConnections = {};
-			this.config = null;
-			var events = {
-				//peer event
-				iceCandidate: "candidate", // {candidate:"",from:""}
-				createOffer: "createOffer", // {sdp:"",from:""}
-				createAnswer: "createAnswer", // {sdp:"",from:""}
-				addStream: "addStream", // {stream:stream,from:""}
-				removeStream: "removeStream", // {stream:stream,from:""}
-				signalingStateChange: "signalingStateChange", // {connectionId:"",state:""}
-				peerConnectionOpen: "peerConnectionOpen", //  {connectionId:""}
-				//local event
-				channelOpen: "channelOpen", //  { channel:"" }
-				error: "error", //{ error:Objcet , errorMessage:""}
-				//server event
-				removeConnection: "removeConnection", // { connectionId:""  }
-				newConnection: "newConnection", // { connectionId:""  }
-				getConnections: "getConnections" // { connectionIds:[] }
-			};
-			this.events = events;
-			var me = this;
-			this.addEventListener = function(type, callback) {
-				if (!this.eventHandlers[type]) this.eventHandlers[type] = [];
-				this.eventHandlers[type].push(callback);
-			};
-
-			this.fireEvent = function(type, data) {
-				data.type = type;
-				console.log("receiveEvent:", data);
-				var callbacks = me.eventHandlers[type];
-				if (callbacks) {
-					for (var i = callbacks.length - 1; i >= 0; i--) {
-						callbacks[i](data);
-					};
-				}
+	function RTCClient() {
+		this.SERVER = {
+			iceServers: [{
+				url: "stun:" + location.hostname + ":9001"
+			}]
+		};
+		//createOffer createAnswer 第三个参数RTCOfferOptions
+		var mediaConstraints = {
+			optional: [],
+			mandatory: {
+				OfferToReceiveAudio: true,
+				OfferToReceiveVideo: true
 			}
+		};
+		this.eventHandlers = {};
+		this.channel = null;
+		this.localStreams = [];
+		this.peerConnections = {};
+		this.config = null;
+		var events = {
+			//peer event
+			iceCandidate: "candidate", // {candidate:"",from:""}
+			createOffer: "createOffer", // {sdp:"",from:""}
+			createAnswer: "createAnswer", // {sdp:"",from:""}
+			addStream: "addStream", // {stream:stream,from:""}
+			removeStream: "removeStream", // {stream:stream,from:""}
+			signalingStateChange: "signalingStateChange", // {connectionId:"",state:""}
+			peerConnectionOpen: "peerConnectionOpen", //  {connectionId:""}
+			//local event
+			channelOpen: "channelOpen", //  { channel:"" }
+			error: "error", //{ error:Objcet , errorMessage:""}
+			//server event
+			removeConnection: "removeConnection", // { connectionId:""  }
+			newConnection: "newConnection", // { connectionId:""  }
+			getConnections: "getConnections" // { connectionIds:[] }
+		};
+		this.events = events;
+		var me = this;
+		this.addEventListener = function(type, callback) {
+			if (!this.eventHandlers[type]) this.eventHandlers[type] = [];
+			this.eventHandlers[type].push(callback);
+		};
 
-			this.attachStream = function(stream, dom) {
-				if (window.URL) { //chrome
-					dom.src = URL.createObjectURL(stream);
-				} else {
-					dom.mozSrcObject = stream;
-				}
-			};
-			/*
+		this.fireEvent = function(type, data) {
+			data.type = type;
+			console.log("receiveEvent:", data);
+			var callbacks = me.eventHandlers[type];
+			if (callbacks) {
+				for (var i = callbacks.length - 1; i >= 0; i--) {
+					callbacks[i](data);
+				};
+			}
+		}
+
+		this.attachStream = function(stream, dom) {
+			if (window.URL) { //chrome
+				dom.src = URL.createObjectURL(stream);
+			} else {
+				dom.mozSrcObject = stream;
+			}
+		};
+		/*
 		*config:{
 			localVideo:dom
 			remoteVideo:dom,//如果有多个远程连接,不能设置此选项,需要监听addStream事件
@@ -72,125 +72,125 @@ var RTCSessionDescription = RTCSessionDescription || mozRTCSessionDescription;
 			channel:ws://192.168.1.161/12345
 		}
 		*/
-			this.start = function(config) {
-				me.config = config;
-				if (typeof WebSocket === 'undefined') {
-					me.fireEvent("error", new {
-						errorMessage: "浏览器不支持WebSocket"
-					});
-					return;
-				}
-				if (getUserMedia) {
-					getUserMedia.call(navigator, config.constraints, function(stream) {
-						me.attachStream(stream, config.localVideo);
-						me.localStreams.push(stream);
-						connect(config.channel);
-					}, function(error) {
-						me.fireEvent("error", {
-							errorMessage: "获取本地媒体失败",
-							error: error
-						});
-					});
-				} else {
+		this.start = function(config) {
+			me.config = config;
+			if (typeof WebSocket === 'undefined') {
+				me.fireEvent("error", {
+					errorMessage: "浏览器不支持WebSocket"
+				});
+				return;
+			}
+			if (getUserMedia) {
+				getUserMedia.call(navigator, config.constraints, function(stream) {
+					me.attachStream(stream, config.localVideo);
+					me.localStreams.push(stream);
+					connect(config.channelUrl);
+				}, function(error) {
 					me.fireEvent("error", {
-						errorMessage: 'webRTC is not yet supported in this browser.'
-					})
+						errorMessage: "获取本地媒体失败",
+						error: error
+					});
+				});
+			} else {
+				me.fireEvent("error", {
+					errorMessage: 'webRTC is not yet supported in this browser.'
+				})
+			}
+		}
+
+		function connect(channelUrl) {
+			var wsChannel = new WebSocket(channelUrl);
+			wsChannel.onopen = function() {
+				me.fireEvent(events.channelOpen, {
+					channel: wsChannel
+				});
+				wsChannel.onmessage = function(msg) {
+					var event = null;
+					try {
+						event = JSON.parse(msg.data);
+					} catch (ex) {
+						console.error("parse message fialed :", msg.data);
+						return;
+					}
+					me.fireEvent(event.type, event);
 				}
 			}
-
-			function connect(channel) {
-				var wsChannel = new WebSocket("ws://" + location.host + "/" + channel);
-				wsChannel.onopen = function() {
-					me.fireEvent(events.channelOpen, {
-						channel: wsChannel
-					});
-					wsChannel.onmessage = function(msg) {
-						var event = null;
-						try {
-							event = JSON.parse(msg.data);
-						} catch (ex) {
-							console.error("parse message fialed :", msg.data);
-							return;
-						}
-						me.fireEvent(event.type, event);
-					}
-				}
-				wsChannel.onclose = function() {
-					console.log("channel[%s] close", channel);
-				}
-
-				wsChannel.onerror = function(err) {
-					console.error("channel[%s] error", err);
-				}
-				me.channel = {
-					send: function(event) {
-						wsChannel.send(JSON.stringify(event));
-						console.log("sendEvent:", event);
-					}
-				};
+			wsChannel.onclose = function() {
+				console.log("channel[%s] close", channel);
 			}
 
-			function removePeer(remoteid) {
-				if (me.peerConnections[remoteid]) {
-					me.peerConnections[remoteid].close();
-					delete me.peerConnections[remoteid];
-					if (me.config.remoteVideo) {
-						me.config.remoteVideo.src = "";
-					}
+			wsChannel.onerror = function(err) {
+				console.error("channel[%s] error", err);
+			}
+			me.channel = {
+				send: function(event) {
+					wsChannel.send(JSON.stringify(event));
+					console.log("sendEvent:", event);
+				}
+			};
+		}
+
+		function removePeer(remoteid) {
+			if (me.peerConnections[remoteid]) {
+				me.peerConnections[remoteid].close();
+				delete me.peerConnections[remoteid];
+				if (me.config.remoteVideo) {
+					me.config.remoteVideo.src = "";
 				}
 			}
+		}
 
-			function createPeerConnection(remoteid) {
-				var pc = new PeerConnection(me.SERVER);
-				me.peerConnections[remoteid] = pc;
-				for (var i = me.localStreams.length - 1; i >= 0; i--) {
-					pc.addStream(me.localStreams[i]);
-				};
-				pc.onicecandidate = function(event) {
-					if (event.candidate) {
-						me.channel.send({
-							type: events.iceCandidate,
-							candidate: event.candidate,
-							to: remoteid
-						});
-					}
-				}
-				pc.onsignalingstatechange = function() {
-					me.fireEvent(events.signalingStateChange, {
-						connectionId: remoteid,
-						state: pc.signalingState
-					});
-				};
-				pc.onopen = function() {
-					me.fireEvent(events.peerConnectionOpen, {
-						connectionId: remoteid
+		function createPeerConnection(remoteid) {
+			var pc = new PeerConnection(me.SERVER);
+			me.peerConnections[remoteid] = pc;
+			for (var i = me.localStreams.length - 1; i >= 0; i--) {
+				pc.addStream(me.localStreams[i]);
+			};
+			pc.onicecandidate = function(event) {
+				if (event.candidate) {
+					me.channel.send({
+						type: events.iceCandidate,
+						candidate: event.candidate,
+						to: remoteid
 					});
 				}
-				pc.onaddstream = function(event) {
-					var remoteVideo = me.config.remoteVideo;
-					if (remoteVideo) {
-						me.attachStream(event.stream, remoteVideo);
-						remoteVideo.id = "remote" + remoteid;
-					}
-					me.fireEvent(events.addStream, {
-						stream: event.stream,
-						from: remoteid
-					});
-				};
-				pc.onremovestream = function(event) {
-					me.fireEvent(events.removeStream, {
-						stream: event.stream,
-						from: remoteid
-					});
-				}
-				return pc;
 			}
+			pc.onsignalingstatechange = function() {
+				me.fireEvent(events.signalingStateChange, {
+					connectionId: remoteid,
+					state: pc.signalingState
+				});
+			};
+			pc.onopen = function() {
+				me.fireEvent(events.peerConnectionOpen, {
+					connectionId: remoteid
+				});
+			}
+			pc.onaddstream = function(event) {
+				var remoteVideo = me.config.remoteVideo;
+				if (remoteVideo) {
+					me.attachStream(event.stream, remoteVideo);
+					remoteVideo.id = "remote" + remoteid;
+				}
+				me.fireEvent(events.addStream, {
+					stream: event.stream,
+					from: remoteid
+				});
+			};
+			pc.onremovestream = function(event) {
+				me.fireEvent(events.removeStream, {
+					stream: event.stream,
+					from: remoteid
+				});
+			}
+			return pc;
+		}
 
 
-			//server events
-			this.addEventListener(events.removeConnection, function(event) {
-					removePeer(event.connectionId);
-			});
+		//server events
+		this.addEventListener(events.removeConnection, function(event) {
+			removePeer(event.connectionId);
+		});
 
 		this.addEventListener(events.newConnection, function(event) {
 			var pc = new PeerConnection(me.SERVER);
@@ -263,8 +263,8 @@ var RTCSessionDescription = RTCSessionDescription || mozRTCSessionDescription;
 		});
 
 		this.addEventListener(events.removeStream, function(event) {
-				removePeer(event.from);
+			removePeer(event.from);
 		});
-}
-exports.RTCClient = RTCClient;
+	}
+	exports.RTCClient = RTCClient;
 })(window);
